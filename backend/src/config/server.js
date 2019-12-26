825cf97bacad7c0e5315a5a46a48c3928e698f6c
unknown <Wallace@WALLACE-PC.marestdt.com>
2019-12-26 17:10:32 -0300

    Adjusting

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,12 +1,12 @@
+require('dotenv').config({ path: `${process.cwd()}/../.env` });
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
 
-require('dotenv').config();
 const port = process.env.APP_PORT;
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
 server.listen(port, () => console.log(`Backend is running on ${port}`));
-
