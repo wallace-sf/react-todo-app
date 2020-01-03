43996a1a82749fa8276a6c28ef585dab99351570
unknown <Wallace@WALLACE-PC.marestdt.com>
2020-01-02 21:25:59 -0300

    Cors added and routes updated

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -10,3 +10,5 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
 server.listen(port, () => console.log(`Backend is running on ${port}`));
+
+module.exports = server;
